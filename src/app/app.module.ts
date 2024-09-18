164472f823d146490f229325727423ef826d7703
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-09-18 16:40:35 -0300

    feat: implementation of personal data validadions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaiorIdadeDirective } from './directives/maior-idade.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent, MessagesComponent, MaiorIdadeDirective],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
